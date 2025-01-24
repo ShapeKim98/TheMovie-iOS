3a00aacdd04d49ef33da14f1d9523211de65f5fb
김도형 <shapekim98@gmail.com>
2025-01-24 15:40:19 +0900

    [chore] #1 프로젝트 생성

---
+++ TheMovie/TheMovie/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TheMovie
+//
+//  Created by 김도형 on 1/24/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
