8e25f236e1efe4222eff5fcf9a25f7bfac1f86d6
김도형 <shapekim98@gmail.com>
2025-01-24 19:02:49 +0900

    [design] #3 디자인 시스템 폰트 추가

---
+++ TheMovie/TheMovie/DesignSystem/Extensions/UIFont+Extension.swift
@@ -0,0 +1,14 @@
+//
+//  UIFont+Extension.swift
+//  TheMovie
+//
+//  Created by 김도형 on 1/24/25.
+//
+
+import UIKit
+
+extension UIFont {
+    static func tm(_ tmFont: TMFont) -> UIFont {
+        return tmFont.uiFont
+    }
+}
---
+++ TheMovie/TheMovie/DesignSystem/Foundation/TMFont.swift
@@ -0,0 +1,28 @@
+//
+//  TMFont.swift
+//  TheMovie
+//
+//  Created by 김도형 on 1/24/25.
+//
+
+import UIKit
+
+enum TMFont {
+    case caption
+    case body
+    case title
+    case headline
+    
+    var uiFont: UIFont {
+        switch self {
+        case .caption:
+            return .systemFont(ofSize: 12, weight: .regular)
+        case .body:
+            return .systemFont(ofSize: 13, weight: .regular)
+        case .title:
+            return .systemFont(ofSize: 15, weight: .bold)
+        case .headline:
+            return .systemFont(ofSize: 16, weight: .bold)
+        }
+    }
+}
