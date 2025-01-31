003413c6a76df6ef921a256e8f071da9dde76476
김도형 <shapekim98@gmail.com>
2025-01-31 20:16:43 +0900

    [feat] #15 네트워크 단절 상황 대응

--- TheMovie/TheMovie/ViewController.swift
+++ TheMovie/TheMovie/ViewController.swift
@@ -6,20 +6,52 @@
 //
 
 import UIKit
+import Network
+
+import SnapKit
 
 final class ViewController: UITabBarController {
-
+    private let toastMessageView = UIView()
+    
+    private let networkMonitor = NetworkMonitor()
+    
+    private var networkIsConnected = true {
+        didSet { didSetNetworkIsConnected() }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        configureUI()
+        
+        configureLayout()
+        
+        networkMonitor.monitoringStart()
+        
+        networkMonitor.monitoringHandler { [weak self] path in
+            guard let `self` else { return }
+            networkIsConnected = path.status == .satisfied
+        }
+    }
+}
+
+// MARK: Configure Views
+private extension ViewController {
+    func configureUI() {
         configureTabBarController()
         
         configureTabBarAppearance()
+        
+        configureToastMessageView()
     }
-
-}
-
-private extension ViewController {
+    
+    func configureLayout() {
+        toastMessageView.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.top.equalToSuperview()
+        }
+    }
+    
     func configureTabBarController() {
         let dayViewController = UINavigationController(rootViewController: DayViewController())
         dayViewController.tabBarItem.title = "CINEMA"
@@ -59,5 +91,37 @@ private extension ViewController {
         tabBar.scrollEdgeAppearance = appearance
         tabBar.tintColor = .tm(.primitive(.blue))
         UITabBar.appearance().scrollEdgeAppearance = appearance
+    }
+    
+    func configureToastMessageView() {
+        let label = UILabel()
+        label.text = "네트워크 연결을 확인해 주세요."
+        label.textColor = .tm(.semantic(.text(.brand)))
+        label.font = .tm(.headline)
+        toastMessageView.addSubview(label)
+        label.snp.makeConstraints { $0.edges.equalToSuperview().inset(8) }
+        
+        toastMessageView.backgroundColor = .tm(.semantic(.background(.tertiary)))
+        toastMessageView.layer.cornerRadius = (UIFont.tm(.headline).lineHeight + 16) / 2
+        toastMessageView.clipsToBounds = true
+        view.addSubview(toastMessageView)
+        toastMessageView.alpha = 0
+        toastMessageView.transform = CGAffineTransform(translationX: 0, y: 0)
+    }
+}
+
+// MARK: Data Bindings
+private extension ViewController {
+    func didSetNetworkIsConnected() {
+        UIView.springAnimate { [weak self] in
+            guard let `self` else { return }
+            if networkIsConnected {
+                toastMessageView.alpha = 0
+                toastMessageView.transform = CGAffineTransform(translationX: 0, y: 0)
+            } else {
+                toastMessageView.alpha = 1
+                toastMessageView.transform = CGAffineTransform(translationX: 0, y: 50)
+            }
+        }
     }
 }
